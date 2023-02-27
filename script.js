83351b6e80cc1006023ca964ed1244dbecfd7b90
machi <carlosintel9@gmail.com>
2023-02-27 03:36:34 -0400

    actual update

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pokeimagefront = document.getElementById("fotito-front")
 const pokeimageback = document.getElementById("fotito-back")
+const pokeid = document.getElementById("pokeid")
 const pokename = document.getElementById("nombre")
 const pokehp = document.getElementById("vida")
 const pokeattack = document.getElementById("ataque")
@@ -7,6 +8,7 @@ const pokedefence = document.getElementById("defensa")
 const pokespecialattack = document.getElementById("ataque-especial")
 const pokespecialdefence = document.getElementById("defensa-especial")
 const pokespeed = document.getElementById("velocidad")
+const poketype = document.getElementById("type")
 
 
 const colores = {
@@ -28,6 +30,10 @@ const colores = {
     fighting: '#2F2F2F',
     default: '#2A1A1F',
 }
+//llamada para que no aparesca el card sin info
+fetch('https://pokeapi.co/api/v2/pokemon/1')
+    .then(data=> data.json())
+    .then(response => renderpokemonData(response))
 //funcion que llama el fetch cuando haces submit en el form
 const searchPokemon = event =>
 {   //event prevent Default es para que el form no haga reset a la pagina
@@ -37,14 +43,18 @@ const searchPokemon = event =>
     fetch(`https://pokeapi.co/api/v2/pokemon/${value.toLowerCase()}`)
     .then(data=> data.json())
     .then(response => renderpokemonData(response))
+    .catch(err => renderNotFound())
+    
 }
 
 const renderpokemonData = data => {
     console.log(data)
 const sprite_front = data.sprites.front_default;
-    pokeimagefront.innerHTML="<img src='"+ sprite_front +"' alt='test' width='150px' height='150px'>";
+    pokeimagefront.innerHTML="<img src='"+ sprite_front +"' alt='couldn't load image' width='150px' height='150px'>";
 const sprite_back = data.sprites.back_default;
-    pokeimageback.innerHTML="<img src='"+ sprite_back +"' alt='test' width='150px' height='150px'>";
+    pokeimageback.innerHTML="<img src='"+ sprite_back +"' alt='couldnt load image' width='150px' height='150px'>";
+const pokeide = data.id;
+    pokeid.innerHTML=pokeide;
 const pokenombre = data.name;
     pokename.innerHTML=pokenombre;
 const pokevida = data.stats[0].base_stat;
@@ -58,5 +68,19 @@ const pokeataqueespecial= data.stats[3].base_stat;
 const pokedefensaespecial= data.stats[4].base_stat;
     pokespecialdefence.innerHTML= pokedefensaespecial;
 const pokevelocidad= data.stats[5].base_stat;
-    pokespeed.innerHTML= pokevelocidad;
+    pokespeed.innerHTML= pokevelocidad; 
+    renderPokemonTypes(data.types);
+}
+
+const renderPokemonTypes = types => {
+    poketype.innerHTML = '';
+    types.forEach(type => {
+        const typeTextElement = document.createElement("div");
+        typeTextElement.style.color = colores[type.type.name];
+        typeTextElement.textContent = type.type.name;
+        poketype.appendChild(typeTextElement);
+    });
+}
+renderNotFound =() =>{
+    alert("Pokemon not found");
 }
